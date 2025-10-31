73451cc5c1c0e0992b8a035d09f2c2f78fa3b8da
BURAK BALTA <082220004@ogr.uludag.edu.tr>
2025-10-31 12:32:17 +0300

    Replace subprocess.call with subprocess.run

--- ram_system.py
+++ ram_system.py
@@ -1,5 +1,3 @@
-# core/ram_system.py
-
 import os
 import subprocess
 from utils import logger
@@ -7,7 +5,7 @@ from utils import logger
 def activate_ram_only_mode():
     logger.log("RAM-only mod başlatılıyor...")
     try:
-        subprocess.call(["swapoff", "-a"])
+        subprocess.run(["swapoff", "-a"], check=False)
         temp_dirs = ["/tmp", "/var/tmp", "/dev/shm"]
         for temp in temp_dirs:
             if os.path.exists(temp):
@@ -17,7 +15,7 @@ def activate_ram_only_mode():
                         if os.path.isfile(full_path):
                             os.remove(full_path)
                         elif os.path.isdir(full_path):
-                            subprocess.call(["rm", "-rf", full_path])
+                            subprocess.run(["rm", "-rf", full_path], check=False)
                     except Exception as e:
                         logger.log(f"{full_path} temizlenemedi: {e}")
         logger.log("Geçici dizinler temizlendi.")
