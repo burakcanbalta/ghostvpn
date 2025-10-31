5a6f172e24cb335d3199252e5aa4f4d407e78636
BURAK BALTA <082220004@ogr.uludag.edu.tr>
2025-10-31 12:20:57 +0300

    Update ghostcloak.py

--- ghostcloak.py
+++ ghostcloak.py
@@ -1,5 +1,3 @@
-# core/ghostcloak.py
-
 from setproctitle import setproctitle
 from utils import logger
 
@@ -7,5 +5,7 @@ def ghost_cloak(name="bash"):
     try:
         setproctitle(name)
         logger.log(f"İşlem adı değiştirildi: {name}")
+        return True
     except Exception as e:
         logger.log(f"GhostCloak hata: {e}")
+        return False
