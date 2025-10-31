e90474dedf3cd4e2d727cb080543fef1b8f70e79
BURAK BALTA <082220004@ogr.uludag.edu.tr>
2025-10-31 12:38:13 +0300

    Change app.run to bind to all interfaces

--- web_panel.py
+++ web_panel.py
@@ -1,5 +1,3 @@
-# web_panel.py
-
 from flask import Flask, jsonify
 import json
 
@@ -15,4 +13,4 @@ def status():
         return jsonify({"error": "Status dosyası bulunamadı."})
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
